91db0207916fdb921805c69f0bdfe48c65d206fe
Prakash s <sp.prakash0209@gmail.com>
2026-05-21 11:00:02 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -23,8 +23,15 @@ provider "aws" {
 }
 
 module "vpc"     { source = "./modules/vpc" }
-module "rds"     { source = "./modules/rds"     vpc_id = module.vpc.vpc_id private_subnet_ids = module.vpc.private_subnet_ids }
-module "lambda"  { source = "./modules/lambda" db_host = module.rds.db_endpoint sqs_url = module.sqs_sns.queue_url sns_arn = module.sqs_sns.topic_arn }
+module "rds"     { source = "./modules/rds" 
+                  vpc_id = module.vpc.vpc_id 
+                  private_subnet_ids = module.vpc.private_subnet_ids }
+module "lambda"  { source = "./modules/lambda" 
+                 db_host = module.rds.db_endpoint 
+                 sqs_url = module.sqs_sns.queue_url 
+                 sns_arn = module.sqs_sns.topic_arn }
 module "sqs_sns" { source = "./modules/sqs-sns" }
-module "ecs"     { source = "./modules/ecs"     vpc_id = module.vpc.vpc_id public_subnet_ids = module.vpc.public_subnet_ids }
+module "ecs"     { source = "./modules/ecs"   
+                  vpc_id = module.vpc.vpc_id 
+                  public_subnet_ids = module.vpc.public_subnet_ids }
 module "cdn"     { source = "./modules/cdn" }
