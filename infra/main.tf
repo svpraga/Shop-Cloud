ea08994db5b1cb6a2ccdbcd9f648ce84282716a3
Prakash s <sp.prakash0209@gmail.com>
2026-05-21 11:04:42 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -5,12 +5,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-random = {
+    random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
-  # Store Terraform state in S3 (team-friendly, Jenkins uses this)
   backend "s3" {
     bucket = "shopcloud-terraform-state-0209"
     key    = "prod/terraform.tfstate"
@@ -22,16 +21,38 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "vpc"     { source = "./modules/vpc" }
-module "rds"     { source = "./modules/rds" 
-                  vpc_id = module.vpc.vpc_id 
-                  private_subnet_ids = module.vpc.private_subnet_ids }
-module "lambda"  { source = "./modules/lambda" 
-                 db_host = module.rds.db_endpoint 
-                 sqs_url = module.sqs_sns.queue_url 
-                 sns_arn = module.sqs_sns.topic_arn }
-module "sqs_sns" { source = "./modules/sqs-sns" }
-module "ecs"     { source = "./modules/ecs"   
-                  vpc_id = module.vpc.vpc_id 
-                  public_subnet_ids = module.vpc.public_subnet_ids }
-module "cdn"     { source = "./modules/cdn" }
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "rds" {
+  source             = "./modules/rds"
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
+
+module "sqs_sns" {
+  source = "./modules/sqs-sns"
+}
+
+module "lambda" {
+  source        = "./modules/lambda"
+  db_host       = module.rds.db_endpoint
+  sqs_url       = module.sqs_sns.queue_url
+  sqs_queue_arn = module.sqs_sns.queue_arn
+  sns_arn       = module.sqs_sns.topic_arn
+}
+
+module "ecs" {
+  source            = "./modules/ecs"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+}
+
+module "cdn" {
+  source = "./modules/cdn"
+}
+
+output "api_gateway_id" {
+  value = module.lambda.api_gateway_id
+}
